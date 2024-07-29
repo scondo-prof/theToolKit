cf500ac1b1a6c8522b8c43d41c84d54864f0befe
condosr <scondo@bearcognition.com>
2024-07-29 17:09:08 -0400

    dev/ layed the groundwork for updating rulesets

--- github/apply_branch_protection_ruleset.py
+++ github/apply_branch_protection_ruleset.py
@@ -7,7 +7,7 @@ from utils import get_github_repositories
 
 load_dotenv()
 
-def check_repo_rulesets(org: str, token: str, repo_name: str):
+def check_repo_rulesets(org: str, token: str, repo_name: str) -> list:
     
 
     url = f"https://api.github.com/repos/{org}/{repo_name}/rulesets"
@@ -19,12 +19,87 @@ def check_repo_rulesets(org: str, token: str, repo_name: str):
     
     response = httpx.get(url, headers=headers)
 
+    rules = response.json()
+
+    return rules
+
+def apply_repo_ruleset(repo_name: str, org: str, token: str) -> None:
+
+    url = f"https://api.github.com/repos/{org}/{repo_name}/rulesets"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    data = {
+   "name":"main-protection",
+   "target":"branch",
+   "enforcement":"active",
+   "bypass_actors":[
+      {
+         "actor_id":1,
+         "actor_type":"OrganizationAdmin",
+         "bypass_mode":"always"
+      }
+   ],
+   "conditions":{
+      "ref_name":{
+         "exclude":[
+            
+         ],
+         "include":[
+            "~DEFAULT_BRANCH"
+         ]
+      }
+   },
+   "rules":[
+      {
+         "type":"deletion"
+      },
+      {
+         "type":"non_fast_forward"
+      },
+      {
+         "type":"pull_request",
+         "parameters":{
+            "required_approving_review_count":1,
+            "dismiss_stale_reviews_on_push":False,
+            "require_code_owner_review":False,
+            "require_last_push_approval":False,
+            "required_review_thread_resolution":False
+         }
+      },
+      {
+         "type":"update"
+      }
+   ]
+}
+    
+    response = httpx.post(url, headers=headers, data=data)
+
+    print(response.status_code)
+    if response.status_code == 200:
+        print(f"-- Successfully Added Ruleset to {repo_name} --")
+    
+    else:
+        print(f"Failed to Add Ruleset to {repo_name} --")
+
+
+
+def get_ruleset_info(repo_name: str, org: str, token: str, ruleset_id: str):
+    url = f"https://api.github.com/repos/{org}/{repo_name}/rulesets/{ruleset_id}"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    
+    response = httpx.get(url, headers=headers)
+
     print(response.json())
 
-def apply_repo_ruleset(repo: str):
-    pass
-
-def main():
+def org_repository_compliance():
     github_secrets = json.loads(os.getenv("github_secrets"))
     org = github_secrets["org"]
     token = github_secrets["token"]
@@ -33,8 +108,25 @@ def main():
 
     for repo in repos:
         repo_name = repo["name"]
+        print(f"-- Checking {repo_name} --")
 
-        check_repo_rulesets(org= org, token=token, repo_name= repo_name)
+        rules = check_repo_rulesets(org= org, token=token, repo_name= repo_name)
+
+        if rules:
+            has_rule = False
+            for rule in rules:
+                if rule["name"] == "main-protection":
+                    has_rule = True
+                    break
+            if has_rule:
+                continue   
+        else:
+            print(f"-- Need to Make Rule --")
+            apply_repo_ruleset(repo_name=repo_name, org=org, token=token)
+
+def main():
+    org_repository_compliance()
+    
 
 
 main()
