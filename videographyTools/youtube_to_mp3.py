3c7f6fe3d8643b9fcd0de157d8a4fc7eb1f29166
Scooter Condo <scondo.prof@gmail.com>
2025-04-03 22:20:04 -0400

    dev/hardcoded url for youtube_to_mp3.py. Need to parameterize

--- videographyTools/youtube_to_mp3.py
+++ videographyTools/youtube_to_mp3.py
@@ -1,7 +1,7 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = "https://www.youtube.com/watch?v=YpKx1tQ6VG0"
+url = "https://www.youtube.com/watch?v=DtcnsPtv_kk"
 
 yt = YouTube(url, on_progress_callback=on_progress)
 print(yt.title)
