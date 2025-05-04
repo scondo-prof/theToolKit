b8f46c85bd2e0bcbf571e4a2fff2881d3f94c178
Scooter Condo <scondo.prof@gmail.com>
2025-05-04 13:56:12 -0400

    dev/got the reccursive async upload functionality working

--- videographyTools/s3_upload.py
+++ videographyTools/s3_upload.py
@@ -11,8 +11,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.files_and_directories import list_files_recursively, move_file
 
 
-async def upload_s3_obj(file_name: str, s3_path: str) -> str:
-    bucket_name = os.getenv("S3_BUCKET")
+async def upload_s3_obj(file_name: str, s3_key: str) -> str:
 
     config = TransferConfig(
         multipart_threshold=1024 * 8,
@@ -21,33 +20,19 @@ async def upload_s3_obj(file_name: str, s3_path: str) -> str:
         use_threads=True,
     )
 
-    print(f"About to upload file with S3 key: {s3_path}")
+    print(f"About to upload file with S3 key: {s3_key}")
     try:
         print("Inside Catch")
         session = aioboto3.Session()
         print("session made")
         async with session.client("s3") as s3_client:
-            response = await s3_client.list_buckets()
-            print(f"Response: {response}")
-            for bucket in response["Buckets"]:
-                print(bucket["Name"])
+            response = await s3_client.upload_file(
+                Filename=file_name, Bucket=os.getenv("S3_BUCKET"), Key=s3_key, Config=config
+            )
+            print(f"Successfully Uploaded: {s3_key}")
 
-    #     async with aioboto3.client(
-    #         "s3",
-    #         config=aioboto3.session.Config(s3={"use_accelerate_endpoint": True}),
-    #     ) as s3_client:
-    #         print("Created the client")
-    #         # Run the blocking upload_file in a thread to avoid blocking the event loop
-    #         await asyncio.to_thread(
-    #             s3_client.upload_file,
-    #             Filename=file_name,
-    #             Bucket=bucket_name,
-    #             Key=s3_path,
-    #             Config=config,
-    #         )
-    #     return f"Successfully Uploaded {s3_path}"
     except Exception as e:
-        return f"Failed Upload for {s3_path}: {e}"
+        return f"Failed Upload for {s3_key}: {e}"
 
 
 async def bulk_s3_upload(s3_path: str) -> list[str]:
@@ -61,14 +46,22 @@ async def bulk_s3_upload(s3_path: str) -> list[str]:
         s3_key = relative_path.replace(os.sep, "/")  # S3 expects forward slashes
         s3_key = s3_path + s3_key
 
-        result = await upload_s3_obj(file_name=file_path, s3_path=s3_key)
+        print(
+            f"""---
+Gathered File: {file_path}
+To be Uploaded To
+S3 Key: {s3_key}"""
+        )
+
+        result = await upload_s3_obj(file_name=file_path, s3_key=s3_key)
         return result
 
     tasks = [upload_task(file_path=file_path, s3_path=s3_path) for file_path in all_files]
+    print("Files Gathered and Ready For Upload")
     results = await asyncio.gather(*tasks)
     return results
 
 
-asyncio.run(upload_s3_obj(file_name="z.txt", s3_path="test/z.txt"))
+# asyncio.run(upload_s3_obj(file_name="z.txt", s3_key="test/z.txt"))
 
 # asyncio.run(bulk_s3_upload(s3_path="test/"))
