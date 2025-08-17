a30093f73d29f39f9b7000c8b197eefbaa33cd09
Scooter Condo <scondo.prof@gmail.com>
2025-08-16 21:21:42 -0400

    dev/modfied youtube_to_mp3.py to take user input, and more verbose print statements

--- videographyTools/youtube_to_mp3.py
+++ videographyTools/youtube_to_mp3.py
@@ -1,10 +1,20 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = "https://www.youtube.com/watch?v=MMihPT3-ADg"
 
-yt = YouTube(url, on_progress_callback=on_progress)
-print(yt.title)
+if __name__ == "__main__":
 
-ys = yt.streams.get_audio_only()
-ys.download(output_path="Q:\\strokeNotPoke\\thePremier\\moosePond\\assets")
+    url: str = input("Enter Desired Video Url: ")
+
+    yt: YouTube = YouTube(url, on_progress_callback=on_progress)
+    print(f"Going to Download: {yt.title}")
+
+    ys = yt.streams.get_audio_only()
+
+    output_path: str = input("Enter Location of Audio Output: ")
+    ys.download(output_path=output_path)
+
+    print(f"Downloaded Video: {yt.title} | To location: {output_path}")
+
+    # https://www.youtube.com/watch?v=MMihPT3-ADg
+    # ./
