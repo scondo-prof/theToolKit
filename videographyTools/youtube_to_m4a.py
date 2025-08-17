9e324e7b9d1851d2dd253d60082419dbb89533cf
Scooter Condo <scondo.prof@gmail.com>
2025-08-16 21:39:43 -0400

    dev/allowed the integration of static output path as an environment variable

---
+++ videographyTools/youtube_to_m4a.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+load_dotenv()
+
+if __name__ == "__main__":
+
+    url: str = input("Enter Desired Video Url: ")
+
+    yt: YouTube = YouTube(url, on_progress_callback=on_progress)
+    print(f"Going to Download: {yt.title}")
+
+    ys = yt.streams.get_audio_only()
+
+    if os.getenv("OUTPUT_PATH"):
+        output_path: str = os.getenv("OUTPUT_PATH")
+
+    else:
+        output_path: str = input("Enter Location of Audio Output: ")
+
+    print(f"Output Location: {output_path}")
+
+    output_file: str = ys.download(
+        output_path=output_path,
+    )
+
+    print(f"Downloaded Video: {yt.title} | To location: {output_file}")
