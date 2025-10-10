05c00e38132a555551f7624f2b1bb2e475c11faf
Scooter Condo <scondo.prof@gmail.com>
2025-10-09 20:07:12 -0400

    dev/started an example script to test a bot simply taking an expected prompt

---
+++ cord_bot/main.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("/test"):
+        await message.channel.send("WE ARE IN HELL")
+
+
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
