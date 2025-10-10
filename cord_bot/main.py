19da95528bc1faaf46ab1479fed1275f6ca5870e
Scooter Condo <scondo.prof@gmail.com>
2025-10-09 20:15:16 -0400

    dev/added return types for functions

--- cord_bot/main.py
+++ cord_bot/main.py
@@ -9,17 +9,18 @@ client = discord.Client(intents=intents)
 
 
 @client.event
-async def on_ready():
+async def on_ready() -> None:
     print(f"We have logged in as {client.user}")
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: str) -> int:
     if message.author == client.user:
-        return
+        return 0
 
     if message.content.startswith("/test"):
         await message.channel.send("WE ARE IN HELL")
+        return 1
 
 
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
