a936dbb1e27cc03a8e2bad49e182925508048f44
Scooter Condo <scondo.prof@gmail.com>
2026-01-12 19:56:04 -0500

    dev/modified so output file uses GITHUB_OUTPUT path. Structured file to use eof syntax

--- .github/workflows/workflow_assets/periodic_issues_notification_format.py
+++ .github/workflows/workflow_assets/periodic_issues_notification_format.py
@@ -2,17 +2,16 @@ from datetime import datetime
 import json
 import os
 
-discord_message = f"""
----
+discord_message = f"""---
 # Issues as of {datetime.now().strftime('%Y-%m-%d')}
 ---------------------------------------------------
 """
-with open("issues.json", "r") as issues_file:
+
+with open("issues.json", "r", encoding="utf-8") as issues_file:
     issues = json.load(issues_file)
 
-    for issue in issues:
-        issue_message = f"""
-
+for issue in issues:
+    discord_message += f"""
 
 ## Issue Title: {issue['number']} - {issue['title']}
 
@@ -26,9 +25,15 @@ Issue Created At: {issue['created_at']}
 Issue Last Update: {issue['updated_at']}
 
 [Issue Link]({issue['html_url']})
- """
-        discord_message += issue_message
+"""
 
-os.environ["GITHUB_OUTPUT"] = f"discord_message=test"
+# GitHub provides the path to the outputs file here:
+output_path = os.environ["GITHUB_OUTPUT"]
 
-print(f" This is the GITHUB_OUTPUT: {os.getenv('GITHUB_OUTPUT')}")
+# Write a multiline output
+with open(output_path, "a", encoding="utf-8") as f:
+    f.write("discord_message<<EOF\n")
+    f.write(discord_message)
+    f.write("\nEOF\n")
+
+print("Wrote discord_message to GITHUB_OUTPUT")
